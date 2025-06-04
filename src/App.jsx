752f87d59463023f358fdcead7c2e44fe35a27fb
hamidsaifi8 <hamidsaifi8@gmail.com>
2025-06-04 10:39:16 +0530

    Category slides

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import CategorySlider from "./components/categorySlider";
+
+function App() {
+  return (
+    <div>
+      <CategorySlider></CategorySlider>
+    </div>
+  );
+}
+
+export default App;
