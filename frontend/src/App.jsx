2cf8cf6fcd16b4dd08e640bbbf0d88407aaf8836
nadeem <nadeemahmad199726@gmail.com>
2025-06-04 20:52:55 +0530

    review updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,7 @@ import Review from "./Components/Review";
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold text-center text-orange-600 mb-10">
-        Navbar
-      </h1>
-      <Hero />
-      <Review/>
+      <Review />
     </div>
   );
 };
